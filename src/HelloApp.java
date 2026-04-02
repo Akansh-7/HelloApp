df47a63b62a0480337f5d4cb44d35976d1254d4c
Akansh-7 <ab0085@srmist.edu.in>
2026-04-02 09:21:18 +0530

    Added required code for UC

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,5 +1,10 @@
-public class UC1 {
+public class UC2 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        if (args.length == 0) {
+            System.out.println("Hello, Prince Raj!");
+            return;
+        }
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
     }
 }
