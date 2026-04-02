6b2f73f3e26b51500fba334a69d81c7093c98776
Akansh-7 <ab0085@srmist.edu.in>
2026-04-02 09:25:01 +0530

    Added required code for UC

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,8 +1,15 @@
-public class UC3{
+public class UC4 {
     public static void main(String[] args) {
         String name;
         if (args.length > 0) {
-            name = args[0];
+            StringBuilder nameBuilder = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+            name = nameBuilder.toString();
         } else {
             name = "World";
         }
